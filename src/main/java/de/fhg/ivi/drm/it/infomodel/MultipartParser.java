e4fae48e9402d6eeb7d107730710af6378aa2f7d
Sebastian Lorenz <sebastian.lorenz@ivi.fraunhofer.de>
2022-04-04 08:21:04 +0200

    Added Apache 2 Licence

--- src/main/java/de/fhg/ivi/drm/it/infomodel/MultipartParser.java
+++ src/main/java/de/fhg/ivi/drm/it/infomodel/MultipartParser.java
@@ -1,16 +1,25 @@
 /*
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+  Copyright 2022 Fraunhofer Institute for Transportation and Infrastructure Systems IVI
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+
+
+  This file has been copied from
+  https://github.com/International-Data-Spaces-Association/IDS-Messaging-Services/blob/3.0.0/messaging/src/main/java/de/fraunhofer/ids/messaging/protocol/multipart/parser/MultipartParser.java
+
+  Adaptions have been made to fit to the file to this project.
+*/
+
 package de.fhg.ivi.drm.it.infomodel;
 
 import lombok.Getter;
